dc7b5dcf5e15f0433790c1fca17d750472df8297
Michael Matloka <michal@matloka.com>
2023-10-31 01:15:19 +0100

    Add temporary compatibility with `ns.<zone>`

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -179,6 +179,13 @@ func (h *DNSHandler) ResolveRRs(question dns.Question) ([]dns.RR, int) {
 				})
 			}
 		}
+	} else if subdomain == "ns" { // ns.<zone> - backwards compatibility
+		switch question.Qtype {
+		case dns.TypeA:
+			records = append(records, &dns.A{
+				A: h.nsA[0],
+			})
+		}
 	} else if subdomain == "alpha" { // alpha.<zone> - first nameserver
 		switch question.Qtype {
 		case dns.TypeA:
